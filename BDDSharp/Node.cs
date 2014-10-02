aa8ac60e7e133d0d54a79c495469ebacbb1e469c
Antoine Cailliau <antoine.cailliau@uclouvain.be>
2014-10-02 09:31:07 +0200

    Implements Apply

--- BDDSharp/Node.cs
+++ BDDSharp/Node.cs
@@ -10,35 +10,35 @@ namespace BDDSharp
     /// </summary>
     public class BDD {
         
-        public Node Root { get; set; }
+        public Node Root { get; private set; }
 
         public Node Zero { get; private set; }
         public Node One { get; private set; }
 
-
-        int variablesCount;
+        public int variablesCount;
         public BDD (int variablesCount)
         {
             this.variablesCount = variablesCount;
-            this.Zero = new Node (0, variablesCount + 1);
-            this.One  = new Node (1, variablesCount + 1);
+            this.Zero = new Node (false, variablesCount);
+            this.One  = new Node (true, variablesCount);
+        }
+
+        public void SetRoot (Node root)
+        {
+            Root = root;
+            root.SetIdentifier (0);
         }
 
         void RedirectEdges (List<Node> nodes, Node oldNode, Node newNode)
         {
             var incomingLow = nodes.Where (x => x.Low != null && x.Low.Identifier == oldNode.Identifier);
-            //Console.WriteLine ("incomingLow for " + oldNode + ":\n" + string.Join ("\n", incomingLow.Select (x => x.ToString ())));
             foreach (var iv in incomingLow)
                 iv.Low = newNode;
             
             var incomingHigh = nodes.Where (x => x.High != null && x.High.Identifier == oldNode.Identifier);
-            //Console.WriteLine ("incomingHigh for " + oldNode + ":\n" + string.Join ("\n", incomingHigh.Select (x => x.ToString ())));
             foreach (var iv in incomingHigh) {
-                //Console.WriteLine ("Update " + iv);
                 iv.High = newNode;
-                //Console.WriteLine ("--> " + iv);
             }
-            //Console.WriteLine ("incomingHigh for " + oldNode + ":\n" + string.Join ("\n", incomingHigh.Select (x => x.ToString ())));
         }
 
         public void Reduce ()
@@ -84,22 +84,85 @@ namespace BDDSharp
             }
         }
 
+        public BDD And (BDD v2)
+        {
+            return BDD.Apply (this, v2, (x, y) => x & y);
+        }
+
+        public BDD Or (BDD v2)
+        {
+            return BDD.Apply (this, v2, (x, y) => x | y);
+        }
+
+        public static BDD Apply (BDD v1, BDD v2, Func<bool?, bool?, bool?> op)
+        {
+            if (v1.variablesCount != v2.variablesCount)
+                throw new ArgumentException ("Cannot apply on BDD not sharing common variable set");
+
+            v1.Root.SetIdentifier(0);
+            v2.Root.SetIdentifier(0);
+
+            var bdd = new BDD (v1.variablesCount);
+            Node[,] cache = new Node[v1.Root.Nodes.Count() + 1, v2.Root.Nodes.Count() + 1];
+            var root = ApplyStep (v1.Root, v2.Root, cache, op, bdd);
+            bdd.SetRoot(root);
+            return bdd;
+        }
+
+        static Node ApplyStep (Node v1, Node v2, Node[,] cache, Func<bool?, bool?, bool?> op, BDD bdd) 
+        {
+            var u = cache[v1.Identifier, v2.Identifier];
+            if (u != null)
+                return u;
+
+            u = new Node (bdd.variablesCount);
+            var tmp = op(v1.Value, v2.Value);
+            if (tmp != null) {
+                var ret = bdd.One;
+                if (!(bool) tmp)
+                    ret = bdd.Zero;
+                cache[v1.Identifier, v2.Identifier] = ret;
+                return ret;
+            }
+            cache[v1.Identifier, v2.Identifier] = u;
+
+            u.Index = Math.Min (v1.Index, v2.Index);
+
+            Node vlow1, vhigh1, vlow2, vhigh2;
+
+            if (v1.Index == u.Index) {
+                vlow1 = v1.Low;
+                vhigh1 = v1.High;
+            } else {
+                vlow1 = v1;
+                vhigh1 = v1;
+            }
+
+            if (v2.Index == u.Index) {
+                vlow2 = v2.Low;
+                vhigh2 = v2.High;
+            } else {
+                vlow2 = v2;
+                vhigh2 = v2;
+            }
+
+            u.Low = ApplyStep (vlow1, vlow2, cache, op, bdd);
+            u.High = ApplyStep (vhigh1, vhigh2, cache, op, bdd);
+
+            return u;
+        }
+
         public object ToDot ()
         {
             var nodes = Root.Nodes.ToList ();
-            int i = 0;
-            foreach (var node in nodes) {
-                node.Identifier = i++;
-            }
-
             var t = new StringBuilder ("digraph G {\n");
             foreach (var n in nodes) {
-                if (n.Value == null) {
-                    t.Append ("\t" + n.Identifier + " [label=\"x" + n.Index + "\"]");
+                if (n.Index <= n.N) {
+                    t.Append ("\t" + n.Identifier + " [label=\"x" + n.Index + " (id:" + n.Identifier + ")\"];\n");
                     t.Append ("\t" + n.Identifier + " -> " + n.High.Identifier + ";\n");
                     t.Append ("\t" + n.Identifier + " -> " + n.Low.Identifier + " [style=dotted];\n");
                 } else {
-                    t.Append ("\t" + n.Identifier + " [shape=box,label=\"" + n.Value + "\"];\n");
+                    t.Append ("\t" + n.Identifier + " [shape=box,label=\"" + (((bool) n.Value) ? "1" : "0") + " (id:" + n.Identifier + ")\"];\n");
                 }
             }
             t.Append ("}");
@@ -110,10 +173,12 @@ namespace BDDSharp
     public class Node {
 
         public int Identifier  { get; set; }
-        public int? Value      { get; set; }
+        public bool? Value     { get; set; }
         public int Index       { get; set; }
         public Node Low        { get; set; }
         public Node High       { get; set; }
+        public bool Mark       { get; set; }
+        public int  N          { get; set; }
 
         public IEnumerable<Node> Nodes {
             get {
@@ -131,20 +196,37 @@ namespace BDDSharp
             }
         }
 
-        public Node (int index, Node high, Node low)
+        public Node (int n)
         {
-            this.Value = null;
+            this.Mark = false;
+            this.N = n;
+        }
+
+        public Node (int index, int n, Node high, Node low) : this(n)
+        {
             this.Index = index;
             this.High = high;
             this.Low = low;
         }
 
-        public Node (int value, int index)
+        public Node (bool value, int n) : this(n)
         {
             this.Value = value;
-            this.Index = index;
+            this.Index = n + 1;
             this.Low = null;
             this.High = null;
+        }
+
+        public int SetIdentifier (int i)
+        {
+            Mark = !Mark;
+            Identifier = i;
+            int tmp = i;
+            if (Low != null && Low.Mark != Mark)
+                tmp = Low.SetIdentifier (tmp + 1);
+            if (High != null && High.Mark != Mark)
+                tmp = High.SetIdentifier (tmp + 1);
+            return tmp;
         }
 
         public override string ToString ()
@@ -154,7 +236,6 @@ namespace BDDSharp
                 Low != null ? Low.Identifier.ToString () : "null", 
                 High != null ? High.Identifier.ToString () : "null");
         }
-        
     }
 }
 
