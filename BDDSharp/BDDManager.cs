85936ad32a32137e1eb20299dcf94613b7748683
Antoine Cailliau <antoine.cailliau@uclouvain.be>
2014-10-03 08:57:18 +0200

    Refactors and documents

---
+++ BDDSharp/BDDManager.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BDDSharp
+{
+    /// <summary>
+    /// Represents a BDD
+    /// </summary>
+    public class BDDManager
+    {
+        /// <summary>
+        /// Gets the node representing zero.
+        /// </summary>
+        /// <value>The node zero.</value>
+        public BDDNode Zero { get; private set; }
+
+        /// <summary>
+        /// Gets the node representing one.
+        /// </summary>
+        /// <value>The node one.</value>
+        public BDDNode One  { get; private set; }
+
+        /// <summary>
+        /// The number of variables
+        /// </summary>
+        public int N;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BDDSharp.BDDManager"/> class.
+        /// </summary>
+        /// <param name="n">The number of variables</param>
+        public BDDManager (int n)
+        {
+            this.N = n;
+            this.Zero = new BDDNode (n + 1, false);
+            this.One = new BDDNode (n + 1, true);
+        }
+
+        /*
+        void RedirectEdges(List<Node> nodes, Node oldNode, Node newNode)
+        {
+            var incomingLow = nodes.Where(x => x.Low != null && x.Low.Identifier == oldNode.Identifier);
+            foreach (var iv in incomingLow)
+                iv.Low = newNode;
+            
+            var incomingHigh = nodes.Where(x => x.High != null && x.High.Identifier == oldNode.Identifier);
+            foreach (var iv in incomingHigh) {
+                iv.High = newNode;
+            }
+        }
+
+        public void Reduce(Node n)
+        {
+            var nodes = n.Nodes.ToList();
+            n.SetIdentifiers(0);
+            for (int i = N; i >= 0; i--) {
+                var Vi = nodes.Where(x => x.Index == i).ToList();
+                var Vi2 = nodes.Where(x => x.Index == i).ToList();
+
+                // Elimination rule
+                foreach (var v in Vi) {
+                    if (v.Low.Identifier == v.High.Identifier) {
+                        Vi2.Remove(v);
+                        RedirectEdges(nodes, v, v.Low);
+                        if (v.Identifier == n.Identifier) {
+                            Root = v.Low;
+                        }
+                    }
+                }
+
+                // Merging rule
+                var oldKey = new Tuple <int, int>(0, 0);
+                Node oldNode = null;
+                foreach (var v in Vi2) {
+                    var key = v.Key;
+                    if (key.Item1 == oldKey.Item1 & key.Item2 == oldKey.Item2) {
+                        //Vi.Remove (v);
+                        RedirectEdges(nodes, v, oldNode);
+                        if (v.Identifier == n.Identifier) {
+                            Root = oldNode;
+                        }
+                    } else {
+                        oldNode = v;
+                        oldKey = v.Key;
+                    }
+                }
+            }
+        }
+        */
+
+        /// <summary>
+        /// Restrict the specified bdd using the < c>positive</ c> and < c>negative</ c> sets.
+        /// </summary>
+        /// <param name="n">Node.</param>
+        /// <param name="positive">Indexes of positive variables.</param>
+        /// <param name="negative">Indexes of negative variables.</param>
+        /// <param name="cache">Cache.</param>
+        public BDDNode Restrict (BDDNode n, ISet<int> positive, ISet<int> negative, Dictionary<BDDNode, BDDNode> cache = null)
+        {
+            if (n.IsOne | n.IsZero)
+                return n;
+
+            if (cache == null)
+                cache = new Dictionary<BDDNode, BDDNode>();
+
+            if (cache.ContainsKey(n))
+                return cache[n];
+
+            BDDNode ret;
+            if (negative.Contains(n.Index))
+                ret = Restrict(n.Low, positive, negative, cache);
+            else if (positive.Contains(n.Index))
+                ret = Restrict(n.High, positive, negative, cache);
+            else {
+                ret = new BDDNode {
+                    Low = Restrict(n.Low, positive, negative, cache),
+                    High = Restrict(n.High, positive, negative, cache)
+                };
+                cache[n] = ret;
+            }
+            return ret;
+        }
+
+        /// <summary>
+        /// Performs the If-Then-Else operation on nodes < c>f</ c>, < c>g</ c>, < c>h</ c>.
+        /// </summary>
+        /// <param name="f">Node.</param>
+        /// <param name="g">Node.</param>
+        /// <param name="h">Node.</param>
+        public BDDNode ITE (BDDNode f, BDDNode g, BDDNode h)
+        {
+            // ite(f, 1, 0) = f
+            if (g.IsOne & h.IsZero)
+                return f;
+
+            // ite(f, 0, 1) = !f
+            if (g.IsZero & h.IsOne)
+                return Negate(f);
+
+            // ite(1, g, h) = g
+            if (f.IsOne)
+                return g;
+
+            // ite(0, g, h) = h
+            if (f.IsZero)
+                return h;
+
+            // ite(f, g, g) = g
+            if (g == h)
+                return g;
+
+            var index = new int[] { f.Index, g.Index, h.Index }.Min();
+            var indexSet = new HashSet<int> { index };
+            var emptySet = new HashSet<int> { };
+
+            var fv0 = Restrict(f, emptySet, indexSet);
+            var gv0 = Restrict(g, emptySet, indexSet);
+            var hv0 = Restrict(h, emptySet, indexSet);
+
+            var fv1 = Restrict(f, indexSet, emptySet);
+            var gv1 = Restrict(g, indexSet, emptySet);
+            var hv1 = Restrict(h, indexSet, emptySet);
+
+            return new BDDNode {
+                Low = ITE(fv0, gv0, hv0),
+                High = ITE(fv1, gv1, hv1),
+            };
+        }
+
+        /// <summary>
+        /// Returns the dot representation of the given node.
+        /// </summary>
+        /// <returns>The dot code.</returns>
+        public object ToDot(BDDNode root)
+        {
+            var nodes = root.Nodes.ToList();
+            var t = new StringBuilder("digraph G {\n");
+            foreach (var n in nodes) {
+                if (n.Index <= N) {
+                    t.Append("\t" + n.Id + " [label=\"x" + n.Index + " (id:" + n.Id + ")\"];\n");
+                    t.Append("\t" + n.Id + " -> " + n.High.Id + ";\n");
+                    t.Append("\t" + n.Id + " -> " + n.Low.Id + " [style=dotted];\n");
+                } else {
+                    t.Append("\t" + n.Id + " [shape=box,label=\"" + (((bool)n.Value) ? "1" : "0") + " (id:" + n.Id + ")\"];\n");
+                }
+            }
+            t.Append("}");
+            return t.ToString();
+        }
+
+        /// <summary>
+        /// Negate the specified node.
+        /// </summary>
+        /// <param name="n">The node.</param>
+        public BDDNode Negate(BDDNode n)
+        {
+            if (n.IsZero)
+                return One;
+            if (n.IsOne)
+                return Zero;
+            return new BDDNode { Low = Negate(n.Low), High = Negate(n.High) };
+        }
+    }
+
+}
+
---
+++ BDDSharp/BDDNode.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BDDSharp
+{
+    /// <summary>
+    /// Represents a BDD node.
+    /// </summary>
+    public class BDDNode
+    {
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>The identifier.</value>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the value.
+        /// </summary>
+        /// <value>The value.</value>
+        public bool? Value { get; set; }
+
+        /// <summary>
+        /// Gets or sets the index.
+        /// </summary>
+        /// <value>The index.</value>
+        public int Index { get; set; }
+
+        /// <summary>
+        /// Gets or sets the low node, i.e. the node to follow when variable <c>index</c> is false.
+        /// </summary>
+        /// <value>The low.</value>
+        public BDDNode Low { get; set; }
+
+        /// <summary>
+        /// Gets or sets the high node, i.e. the node to follow when variable <c>index</c> is true.
+        /// </summary>
+        /// <value>The high.</value>
+        public BDDNode High { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this <see cref="BDDSharp.BDDNode"/> has been visited or not.
+        /// </summary>
+        /// <value><c>true</c> if visited; otherwise, <c>false</c>.</value>
+        public bool Mark { get; set; }
+
+        /// <summary>
+        /// Gets all the nodes, including descendants.
+        /// </summary>
+        /// <value>The nodes.</value>
+        public IEnumerable<BDDNode> Nodes {
+            get {
+                if (Low == null && High == null) {
+                    return new [] { this };
+                } else {
+                    return new [] { this }.Union(Low.Nodes.Union(High.Nodes));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets the key composed by <c>(Low.Id, High.Id)</c>
+        /// </summary>
+        /// <value>The key.</value>
+        public Tuple<int, int> Key {
+            get {
+                return new Tuple <int, int>(Low.Id, High.Id);
+            }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether this instance is the node one.
+        /// </summary>
+        /// <value><c>true</c> if this instance is one; otherwise, <c>false</c>.</value>
+        public bool IsOne {
+            get {
+                return Value != null && ((bool)Value) == true;
+            }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether this instance is the node zero.
+        /// </summary>
+        /// <value><c>true</c> if this instance is zero; otherwise, <c>false</c>.</value>
+        public bool IsZero {
+            get { return Value != null && ((bool)Value) == false; }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BDDSharp.BDDNode"/> class.
+        /// </summary>
+        public BDDNode ()
+        {
+            this.Mark = false;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BDDSharp.BDDNode"/> class.
+        /// </summary>
+        /// <param name="index">Index of the variable the node represents</param>
+        /// <param name="high">The high node (aka 1-node).</param>
+        /// <param name="low">The low node (aka 0-node).</param>
+        public BDDNode (int index, BDDNode high, BDDNode low) : this ()
+        {
+            this.Index = index;
+            this.High = high;
+            this.Low = low;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BDDSharp.BDDNode"/> class.
+        /// </summary>
+        /// <param name="index">The index for the sink node (shall be <c>n+1</c> where <c>n</c> is the number of variables).</param>
+        /// <param name="value">Value represented by the sink node.</param>
+        public BDDNode (int index, bool value) : this ()
+        {
+            this.Value = value;
+            this.Index = index;
+            this.Low = null;
+            this.High = null;
+        }
+
+        /// <summary>
+        /// Sets the identifiers of this node and its descendant.
+        /// </summary>
+        /// <returns>The identifiers.</returns>
+        /// <param name="i">The index to start from.</param>
+        public int SetIdentifiers(int i = 0)
+        {
+            Mark = !Mark;
+            Id = i;
+            int tmp = i;
+            if (Low != null && Low.Mark != Mark)
+                tmp = Low.SetIdentifiers(tmp + 1);
+            if (High != null && High.Mark != Mark)
+                tmp = High.SetIdentifiers(tmp + 1);
+            return tmp;
+        }
+
+        /// <summary>
+        /// Returns a <see cref="System.String"/> that represents the current <see cref="BDDSharp.BDDNode"/>.
+        /// </summary>
+        /// <returns>A <see cref="System.String"/> that represents the current <see cref="BDDSharp.BDDNode"/>.</returns>
+        public override string ToString()
+        {
+            return string.Format("[Node: Identifier={0}, Value={1}, Index={2}, Low={3}, High={4}]", 
+                Id, Value, Index, 
+                Low != null ? Low.Id.ToString() : "null", 
+                High != null ? High.Id.ToString() : "null");
+        }
+    }
+}
+
